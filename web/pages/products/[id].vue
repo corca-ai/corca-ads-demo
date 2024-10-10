b9f93e7da219b954b1bce0114ae2a6c2c6fc8a4c
dev-redo <mis05041@naver.com>
2024-10-10 12:21:39 +0900

    feat: init

---
+++ web/pages/products/[id].vue
@@ -0,0 +1,68 @@
+<script setup>
+import { useNuxtApp, callWithNuxt } from "#app";
+
+// Nuxt의 미들웨어를 사용하여 SSR에서 상품 디테일 데이터를 fetch합니다.
+definePageMeta({
+  middleware: [
+    async (to) => {
+      const nuxtApp = useNuxtApp();
+      const { data: photo } = await useFetchWithBaseUrl(
+        `photos/${to.params.id}`
+      );
+      await callWithNuxt(nuxtApp, () => {
+        nuxtApp.$requestData = photo.value;
+      });
+    },
+  ],
+});
+
+const nuxtApp = useNuxtApp();
+const photo = ref(nuxtApp.$requestData);
+
+// 페이지가 마운트되면 PageView 이벤트 API를 호출합니다.
+onMounted(() => {
+  // TODO: PageView API POST
+  console.log("PageView API POST");
+});
+
+// 장바구니 버튼을 클릭하면 장바구니 API를 호출합니다.
+const handleAddToCart = async (photoId) => {
+  console.log(`Add to Cart API POST: ${photoId}`);
+};
+
+// 구매 버튼을 클릭하면 구매 API를 호출합니다.
+const handlePurchase = async (photoId) => {
+  console.log(`Purchase API POST: ${photoId}`);
+};
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-full h-screen p-8 bg-gray-100">
+    <div
+      class="flex flex-col items-center w-4/5 max-w-3xl bg-white p-6 rounded-lg shadow-md"
+    >
+      <img
+        :src="`${photo.url}`"
+        alt="Photo Avatar"
+        width="100%"
+        height="100%"
+        class="w-full mb-4 rounded-md"
+      />
+      <div class="text-xl font-semibold mb-4">{{ photo.title }}</div>
+      <div class="flex gap-4">
+        <button
+          @click="handleAddToCart(photo.id)"
+          class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
+        >
+          장바구니
+        </button>
+        <button
+          @click="handlePurchase(photo.id)"
+          class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition"
+        >
+          구매
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
