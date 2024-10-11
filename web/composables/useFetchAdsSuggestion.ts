c9b6710e06bd03434e0fc46a8e164cbd138c4eda
dev-redo <mis05041@naver.com>
2024-10-11 21:48:05 +0900

    wip

--- web/composables/useFetchAdsSuggestion.ts
+++ web/composables/useFetchAdsSuggestion.ts
@@ -1,6 +1,4 @@
 export const useFetchAdsSuggestion: typeof useFetch = (request, opts?) => {
-  const config = useRuntimeConfig();
-
   return useFetch(request, {
     baseURL: "https://jsonplaceholder.typicode.com/",
     ...opts,
