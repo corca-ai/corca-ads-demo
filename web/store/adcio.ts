9254bec2b1ab705466a39a8c2ed9e116d0258caf
dev-redo <mis05041@naver.com>
2024-10-11 22:24:30 +0900

    refactor: useGlobalSessionId 리팩터링

--- web/store/adcio.ts
+++ web/store/adcio.ts
@@ -8,28 +8,35 @@ export const useGlobalDeviceId = createGlobalState(() => {
   return useStorage(`adcio-device-${storeId}`, createUUID());
 });
 
+const EXPIRATION_STORAGE = 30 * 60 * 1000; // session id 만료 시간: 30분
 export const useGlobalSessionId = createGlobalState(() => {
   const config = useRuntimeConfig();
   const storeId = config.public.storeId;
+
   const storageKey = `adcio-session-${storeId}`;
-  const expirationTime = 30 * 60 * 1000;
+  const expirationKey = `${storageKey}-expiration`;
 
-  return useSessionStorage(storageKey, () => {
+  const getSessionId = () => {
+    // useSessionStorage를 사용하여 sessionId와 expiration을 관리
+    const sessionIdStorage = useSessionStorage(storageKey, "");
+    const expirationStorage = useSessionStorage(expirationKey, "");
+
     const currentTime = Date.now();
-    const sessionExpiration =
-      Number(sessionStorage.getItem(`${storageKey}-expiration`)) || 0;
+    const sessionExpiration = Number(expirationStorage.value) || 0;
 
-    if (currentTime > sessionExpiration) {
-      const newSessionId = createUUID();
-      sessionStorage.setItem(
-        `${storageKey}-expiration`,
-        String(currentTime + expirationTime)
-      );
-      return newSessionId;
+    if (currentTime <= sessionExpiration && sessionIdStorage.value) {
+      return sessionIdStorage.value;
     }
 
-    return sessionStorage.getItem(storageKey) || createUUID();
-  });
+    // sessionId가 30분이 지나 만료되었거나 존재하지 않는 경우 새로 생성 및 저장
+    const sessionId = createUUID();
+    sessionIdStorage.value = sessionId;
+    expirationStorage.value = String(currentTime + EXPIRATION_STORAGE);
+
+    return sessionId;
+  };
+
+  return getSessionId();
 });
 
 const createUUID = () => crypto.randomUUID();
